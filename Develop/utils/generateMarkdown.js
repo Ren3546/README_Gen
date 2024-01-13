ba07dfb63827e3c5149dfa381fae8dc3e7bdca68
Rengo _ <renatovaldez6879@gmail.com>
2024-01-13 13:11:49 -0500

    fixed markdown with listing table of contents

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -37,13 +37,13 @@ ${renderLicenseBadge(data.license)}
   ## Description
     ${data.description}
   ## Table of Contents
-    * Description (#description)
-    * Installation (#installation)
-    * Usage (#usage)
-    * License (#license)
-    * Contributing (#contributing)
-    * Tests (#tests)
-    * Questions (#questions)
+    * [Description] (#description)
+    * [Installation] (#installation)
+    * [Usage] (#usage)
+    * [License] (#license)
+    * [Contributing] (#contributing)
+    * [Tests] (#tests)
+    * [Questions] (#questions)
   ## Installation
     ${data.installation}
   ## Usage
