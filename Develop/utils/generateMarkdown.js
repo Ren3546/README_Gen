80822e6c57b594b7fedfdbab9f631337b0f44e2d
Rengo _ <renatovaldez6879@gmail.com>
2024-01-13 12:09:35 -0500

    function for markdown

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,19 +1,57 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+//returns license badge
+function renderLicenseBadge(license) {
+  if (!license) {
+    return ('')
+  } else {
+    return `[![License: ${license}](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)`
+  }
+}
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+//returns license link 
+function renderLicenseLink(license) {
+  if (!license) {
+    return ('')
+  } else {
+    return `https://opensource.org/license/${license}/`
+    }
+  }
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+//returns license statement of README
+function renderLicenseSection(license) {
+  if (!license) {
+    return ('')
+  } else {
+    return `this project is ${license} licensed.`
+  }
+}
 
-// TODO: Create a function to generate markdown for README
+//function to create markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
-
+${renderLicenseBadge(data.license)}
+  ## Description
+    ${data.description}
+  ## Table of Contents
+    * Description (#description)
+    * Installation (#installation)
+    * Usage (#usage)
+    * License (#license)
+    * Contributing (#contributing)
+    * Tests (#tests)
+    * Questions (#questions)
+  ## Installation
+    ${data.installation}
+  ## Usage
+    ${data.usage}
+  ## License 
+    ${renderLicenseSection(data.license)}
+    ${renderLicenseLink(data.license)}
+  ## Contributing 
+    ${data.contribute}
+  ## Tests 
+    ${data.tests}
+## Questions 
+    ${data.questions}   
 `;
 }
 
